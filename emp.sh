d449990cc3a784ce507b02f668f9c0a453d3864a
pankaj kore <korep95@gmail.com>
2021-12-28 21:29:12 +0530

    [pankaj]added checkAttendance

--- emp.sh
+++ emp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash/ -x
 echo "welcome to employee wage computation"
+isPresent='1'
+checkAttendance=$((RANDOM%2))
+
+if [ $checkAttendance == $isPresent ]
+	then
+		echo "employee is present"
+	else
+		echo "employee is absent"
+fi
