87df2d02c7090d6813d7a08e4e73b192842b8264
pankaj kore <korep95@gmail.com>
2021-12-28 21:51:45 +0530

    [pankaj] added the calculation of emp wage for one day

--- emp.sh
+++ emp.sh
@@ -2,10 +2,13 @@
 echo "welcome to employee wage computation"
 isPresent='1'
 checkAttendance=$((RANDOM%2))
-
+WagePerHour=20
+FullDayHour=8
 if [ $checkAttendance == $isPresent ]
 	then
 		echo "employee is present"
+		value=$(( $FullDayHour * $WagePerHour ))
+		echo $value
 	else
 		echo "employee is absent"
 fi
