72695c3eefa06c76d2ca0d2985277abaa8405e08
pankaj kore <korep95@gmail.com>
2021-12-29 00:02:58 +0530

    [pankaj]implemented the check for working hour

--- emp.sh
+++ emp.sh
@@ -4,10 +4,12 @@ isPresent='1'
 isParttime='2'
 WagePerHour=20
 FullDayHour=8
-ParttimeWorkingHour=4
+ParttimeWorkingHour=8
 WorkedDaysForMonth=20
 WorkedDay=1
 TotalSalary=0
+PreviousTotalWorkingHour=0
+TotalWorkingHourPossible=100
 
 function WageCalculation(){
 	local workingHourPerDay=$1
@@ -17,22 +19,23 @@ function WageCalculation(){
 	TotalSalaryWorkedDaysForMonth=$(( $value + $TotalSalaryWorkedDaysForMonth ))
 	echo  $TotalSalaryWorkedDaysForMonth
 }
-while [ $WorkedDay -le $WorkedDaysForMonth ]
+while [ $WorkedDay -le $WorkedDaysForMonth ] && [ $PreviousTotalWorkingHour -le $TotalWorkingHourPossible ]
 do
 checkAttendance=$((RANDOM%3))
 	case $checkAttendance in
 		$isPresent)
 			echo "employee is present"
                 	TotalSalary=`WageCalculation $FullDayHour $WagePerHour $TotalSalary`
-			((WorkedDay++))
+			PreviousTotalWorkingHour=$(($PreviousTotalWorkingHour + $FullDayHour ))
 			echo $TotalSalary;;
 		$isParttime)
 			echo "employee is doing partime"
                 	TotalSalary=`WageCalculation $ParttimeWorkingHour $WagePerHour $TotalSalary`
-			((TotalSalary++))
+			PreviousTotalWorkingHour=$(($PreviousTotalWorkingHour + $ParttimeWorkingHour ))
 			echo $TotalSalary;;
 
 		*)
 			echo "employee is absent"
 	esac
+((WorkedDay++))
 done
