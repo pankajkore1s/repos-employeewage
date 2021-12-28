6e43a7e7a79174939d6df44c691cc58f694a328e
pankaj kore <korep95@gmail.com>
2021-12-28 22:29:26 +0530

    [pankaj] added parttime wage calc

--- emp.sh
+++ emp.sh
@@ -1,14 +1,28 @@
 #!/bin/bash/ -x
 echo "welcome to employee wage computation"
 isPresent='1'
-checkAttendance=$((RANDOM%2))
+isParttime='2'
+checkAttendance=$((RANDOM%3))
 WagePerHour=20
 FullDayHour=8
+ParttimeWorkingHour=4
+
+function WageCalculation(){
+	local workingHourPerDay=$1
+	local WageRecievedPerHour=$2
+	value=$(( $workingHourPerDay * $WageRecievedPerHour ))
+	echo $value
+}
+
 if [ $checkAttendance == $isPresent ]
 	then
 		echo "employee is present"
-		value=$(( $FullDayHour * $WagePerHour ))
-		echo $value
+		`echo WageCalculation $FullDayHour $WagePerHour`
+	
+	elif [ $checkAttendance == $isParttime ]
+	then
+		echo "employee is doing partime"
+		`echo WageCalculation $ParttimeWorkingHour $WagePerHour`
 	else
 		echo "employee is absent"
 fi
